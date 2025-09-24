d8c94130b70007724d5919abc0f893bb2b04409b
enara28 <enara.zeamar@gmail.com>
2025-09-24 10:29:47 +0200

    estructura base de la app con login

--- app.py
+++ app.py
@@ -10,7 +10,7 @@
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 # db = SQLAlchemy(app)
 # ma = Marshmallow(app)
-# # app.config["JWT_SECRET_KEY"] = "my_super_secret_key" #cambia esto
+# # app.config["JWT_SECRET_KEY"] = 'Wcw4xvlkDAYX-L5c' #cambia esto
 # # jwt = JWTManager(app)
 
 # class User(db.Model):
@@ -117,19 +117,27 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 import os
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config["JWT_SECRET_KEY"] = 'Wcw4xvlkDAYX-L5c'
+app.config['JWT_VERIFY_SUB'] = False
+app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies", "json", "query_string"]
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+jwt = JWTManager(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=False)
-    email = db.Column(db.String(144), unique=True)
+    email = db.Column(db.String(144), unique=True,)
     password = db.Column(db.String(144), unique=False)
 
     def __init__(self, username, email, password):
@@ -139,6 +147,7 @@ class User(db.Model):
 
 
 class UserSchema(ma.Schema):
+    id = ma.Integer()
     username = ma.String()
     email = ma.String()
     password = ma.String()
@@ -149,7 +158,8 @@ users_schema = UserSchema(many=True)
 
 @app.route("/")
 def greeting():
-    return "Hello Itzala"
+    return "Bienvenido al portal api de Itzala"
+
 
 # Endpoint to create a new guide
 @app.route('/user', methods=["POST"])
@@ -167,18 +177,44 @@ def add_users():
 
     return user_schema.jsonify(user)
 
+# Endpoint to log in with an existing user
+@app.route("/login", methods=["POST"])
+def login():
+    password = request.json.get("password")
+    email = request.json.get("email")
+
+    user = db.session.execute(db.select(User).filter_by(email=email)).scalar_one_or_none()
+
+
+    if user and user.password == password and user.email == email:
+        access_token = create_access_token(identity={"email": email, "password": password})
+        return jsonify(access_token=access_token)
+    else:
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    
+
+# ruta a la carpeta: "C:\Users\Enara\OneDrive\Escritorio\itzala_back\segunda_prueba_pipenv"
+
+# @app.route("/protected", methods=["GET"])
+# @jwt_required()
+# def protected():
+#     current_user = get_jwt_identity()
+#     return jsonify(logged_in_as=current_user), 200
 
 # Endpoint to query all guides
 @app.route("/users", methods=["GET"])
+@jwt_required()
 def get_users():
+    current_user = get_jwt_identity()
     all_users = User.query.all()
     result = users_schema.dump(all_users)
-    return jsonify(result)
+    return jsonify(result, current_user), 200, {'Access-Control-Allow-Origin':'*'}
 
 @app.route("/user/<id>", methods=["GET"])
-def get_guide(id):
+def get_user(id):
     user = User.query.get(id)
-    return user_schema.jsonify(user)
+    return user_schema.jsonify(user), 200, {'Access-Control-Allow-Origin':'*'}
 
 # Endpoint for updating a guide
 @app.route("/user/<id>", methods=["PUT"])
@@ -206,3 +242,5 @@ def user_delete(id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Para generar secret key en python rapl: 1. import os, os.urandom(12) 2. import secrets, secrets.token_urlsafe(12)
